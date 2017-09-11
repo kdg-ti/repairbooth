c360cdcf07e8c1f9acbaa0bb55c3dc5765ce16b1
Wouter <wouter.deketelaere@kdg.be>
2017-09-11 19:06:19 +0200

    start

---
+++ src/main/java/be/kdg/repaircafe/RepairCafeApplication.java
@@ -0,0 +1,12 @@
+package be.kdg.repaircafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairCafeApplication.class, args);
+    }
+}
