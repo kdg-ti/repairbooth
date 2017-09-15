f3f0df6b70f7abefc1ed6e0ef2043cb5eaaa007b
Wouter Deketelaere <wouter.deketelaere@kdg.be>
2017-09-15 10:49:26 +0200

    Toevoegen van extra Repair Repository interface methoden die gebruik maken van @Query

--- src/main/java/be/kdg/repaircafe/persistence/api/RepairRepository.java
+++ src/main/java/be/kdg/repaircafe/persistence/api/RepairRepository.java
@@ -2,12 +2,25 @@ package be.kdg.repaircafe.persistence.api;
 
 import be.kdg.repaircafe.dom.repairs.Repair;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Interface to trigger proxy creation of CRUD Repository
+ * <p>
+ * https://programmeren3-repaircafe.rhcloud.com/repair-cafe-applicatie/repair-cafe-backend/backend-persistence-layer/
  *
  * @author wouter
  */
-public interface RepairRepository extends JpaRepository<Repair, Integer>, RepairRepositoryCustom{
-    // https://programmeren3-repaircafe.rhcloud.com/repair-cafe-applicatie/repair-cafe-backend/backend-persistence-layer/
+public interface RepairRepository extends JpaRepository<Repair, Integer>, RepairRepositoryCustom {
+
+    @Query(value = "SELECT distinct r.item.category FROM Repair r")
+    List<String> getAllCategories();
+
+    @Query(value = "select distinct r.details.defect from Repair r")
+    List<String> getAllDefects();
+
+    @Query(value = "select distinct r.item.brand from Repair r")
+    List<String> getAllBrands();
 }
